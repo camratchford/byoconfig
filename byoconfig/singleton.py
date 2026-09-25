451cd6ff99f5b13559b08a72521c18a51563cf47
Cam Ratchford <camratchford@gmail.com>
2026-09-25 13:39:55 -0600

    Use RLock in SingletonMetaclass so singletons can create other singletons during init.

--- byoconfig/singleton.py
+++ byoconfig/singleton.py
@@ -1,11 +1,11 @@
 import threading
 
-_singleton_lock = threading.Lock()
+_singleton_lock = threading.RLock()
 
 
 class SingletonMetaclass(type):
     def __call__(cls, *args, **kwargs):
         with _singleton_lock:
-            if not hasattr(cls, "_instance"):
+            if "_instance" not in cls.__dict__:
                 cls._instance = super().__call__(*args, **kwargs)
         return cls._instance
